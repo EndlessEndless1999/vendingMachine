1caa3dde1ab1a83d5b2cecdc4ccaecbf8d7a0599
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2024-11-01 13:07:29 +0000

    adds coin management

--- vendoMatic/src/Item.java
+++ vendoMatic/src/Item.java
@@ -1,2 +1,6 @@
 public interface Item {
+    public void name();
+    public void id();
+
+
 }
--- vendoMatic/src/Machine.java
+++ vendoMatic/src/Machine.java
@@ -1,12 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Machine {
 
-    private Float money;
+    private List<Coin> money;
     private User currentUser;
 
     public Machine(Float money, User currentUser) {
-        this.money = money;
+        this.money = new ArrayList<Coin>();
         this.currentUser = currentUser;
     }
 
@@ -15,15 +17,19 @@ public class Machine {
         System.out.println("1 : Insert Credit.");
         System.out.println("2 : Choose Product.");
         System.out.println("3 : View Credit.");
+        System.out.println("4 : Refund Credit");
 
         int input = this.getInput();
 
         switch (input) {
-            case 1 -> this.setCredit();
+            case 1 -> this.addCredit();
             case 2 -> this.chooseProduct();
             case 3 -> this.getCredit();
+            case 4 -> this.refund();
         }
+        displayOptions();
     }
+
 
     private void chooseProduct() {
         System.out.println("You are now viewing current products in the Machine");
@@ -31,10 +37,27 @@ public class Machine {
 
     private void getCredit() {
         System.out.println("You are now viewing current credits in the Machine");
+        float total = 0f;
+        for (Coin coin : this.money) {
+            total += coin.getValue();
+
+        }
+        System.out.println("£" + total);
     }
 
-    private void setCredit() {
+    private void addCredit() {
         System.out.println("You are now Inserting Credits into the Machine");
+        Coin coin = new Coin("Sterling", 1.00F, "£1");
+
+        this.money.add(coin);
+
+        System.out.println(this.money);
+
+        this.getCredit();
+    }
+
+    private void refund() {
+        System.out.println();
     }
 
     public int getInput() {
@@ -42,7 +65,7 @@ public class Machine {
         return scanner.nextInt();
     }
 
-    public Float getMoney() {
+    public List<Coin> getMoney() {
         return money;
     }
 
